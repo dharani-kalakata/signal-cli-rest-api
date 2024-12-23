3bdd040a95a3fa863dfe68eb461d72f803787800
Dharani Bodanapu <140893776+dharani-kalakata@users.noreply.github.com>
2024-12-23 12:05:14 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,35 +1,26 @@
 #!/bin/sh
-
-set -x
 set -e
 
-[ -z "${SIGNAL_CLI_CONFIG_DIR}" ] && echo "SIGNAL_CLI_CONFIG_DIR environmental variable needs to be set! Aborting!" && exit 1;
-
-usermod -u ${SIGNAL_CLI_UID} signal-api
-groupmod -o -g ${SIGNAL_CLI_GID} signal-api
-
-# Fix permissions to ensure backward compatibility
-chown ${SIGNAL_CLI_UID}:${SIGNAL_CLI_GID} -R ${SIGNAL_CLI_CONFIG_DIR}
-
-# Show warning on docker exec
-cat <<EOF >> /root/.bashrc
-echo "WARNING: signal-cli-rest-api runs as signal-api (not as root!)" 
-echo "Run 'su signal-api' before using signal-cli!"
-echo "If you want to use signal-cli directly, don't forget to specify the config directory. e.g: \"signal-cli --config ${SIGNAL_CLI_CONFIG_DIR}\""
-EOF
-
-cap_prefix="-cap_"
-caps="$cap_prefix$(seq -s ",$cap_prefix" 0 $(cat /proc/sys/kernel/cap_last_cap))"
-
-# TODO: check mode
-if [ "$MODE" = "json-rpc" ]
-then
-/usr/bin/jsonrpc2-helper
-if [ -n "$JAVA_OPTS" ] ; then
-    echo "export JAVA_OPTS='$JAVA_OPTS'" >> /etc/default/supervisor
+# Check if the signal-api user exists, if not, create it
+if ! id "signal-api" >/dev/null 2>&1; then
+    useradd -m -u 1000 signal-api
 fi
-service supervisor start
-supervisorctl start all
+
+# Proceed with the rest of the script
+if [ -z "$SIGNAL_CLI_HOME" ]; then
+    SIGNAL_CLI_HOME=/home/.local/share/signal-cli
+fi
+
+usermod -u 1000 signal-api
+
+# Add any other necessary commands here
+if [ -n "$SIGNAL_CLI_HOME" ]; then
+    /usr/bin/jsonrpc2-helper
+    if [ -n "$JAVA_OPTS" ] ; then
+        echo "export JAVA_OPTS='$JAVA_OPTS'" >> /etc/default/supervisor
+    fi
+    service supervisor start
+    supervisorctl start all
 fi
 
 export HOST_IP=$(hostname -I | awk '{print $1}')
